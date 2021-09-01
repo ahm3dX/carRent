1077c25e81ea7d3a87481ca1cab01278506a6c9f
ahm3dX <43940347+ahm3dX@users.noreply.github.com>
2021-09-01 21:12:21 +0100

    Add files via upload

---
+++ src/car/test.java
@@ -0,0 +1,32 @@
+
+package car;
+
+
+public class test {
+    
+    
+    
+    
+    public static void main(String args[])
+    
+    {
+    String status = "yes";
+        
+          if( status == "ye")
+          {
+              System.out.println("A");
+          }
+    else
+          {
+              System.out.println("B");
+          }
+            
+        
+        
+    }
+  
+
+}
+    
+    
+
